de9e3218ccc2aa57b653ad162508c29913ff4ad6
이삭 <alwlsdydtjd1@gmail.com>
2022-05-11 17:09:43 +0900

    이삭 : 다중 프로세서 컴파일 설정 추가

--- Portfolio/GameEngineApp/MyGame.cpp
+++ Portfolio/GameEngineApp/MyGame.cpp
@@ -1,7 +1,13 @@
 #include <Windows.h>
+#include <GameEngineBase/GameEngineDebug.h>
+#include <GameEngineBase/GameEngineDirectory.h>
+#include <GameEngineBase/GameEngineSound.h>
 
+#pragma commet(lib, "GameEngineBase.lib");
 
 int main()
 {
+	int a = 0;
+
 	return 0;
 }
