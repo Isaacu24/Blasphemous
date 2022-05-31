0f98d362c5e139c051bdc4021f23359e0a17a7ea
이삭 <alwlsdydtjd1@gmail.com>
2022-05-31 20:49:27 +0900

    이삭 : 트랜스폼 상속 컴포넌트 추가

--- Portfolio/GameEngineBase/GameEngineTransform.cpp
+++ Portfolio/GameEngineBase/GameEngineTransform.cpp
@@ -1,7 +1,10 @@
 #include "GameEngineTransform.h"
 
 GameEngineTransform::GameEngineTransform()
-	: Scale(float4::ONE)
+	: LocalScale(float4::ONE)
+	, LocalPosition(float4::ZERO)
+	, LocalRotation(float4::ZERO)
+	, Parent(nullptr)
 {
 }
 
@@ -9,3 +12,24 @@ GameEngineTransform::~GameEngineTransform()
 {
 }
 
+
+void GameEngineTransform::CalculateWorld()
+{
+	//로컬 크기
+	LocalWorldMat = LocalScaleMat * LocalRotateMat * LocalPositionMat;
+
+	if (nullptr != Parent) //부모가 있을 경우 부모의 크기를 곱한다
+	{
+		WorldWorldMat = LocalWorldMat * Parent->GetWorldWorld();
+	}
+	else
+	{
+		WorldWorldMat = LocalWorldMat;
+	}
+}
+
+void GameEngineTransform::PushChild(GameEngineTransform* _Child)
+{
+	_Child->Parent = this;
+	Childs.push_back(_Child);
+}
--- Portfolio/GameEngineContents/Otus.cpp
+++ Portfolio/GameEngineContents/Otus.cpp
@@ -12,8 +12,6 @@ Otus::~Otus()
 
 void Otus::Start()
 {
-	GetTransform().SetScale({ 100, 50, 100 });
-	GetTransform().SetPosition({ 300, 300, 100 });
 	{
 		GameEngineRenderer* Renderer = CreateComponent<GameEngineRenderer>();
 		// Renderer->ChangeOrder();
@@ -31,9 +29,6 @@ void Otus::Update(float _DeltaTime)
 {
 	TimeAngle += _DeltaTime * 20.0f;
 
-	GetTransform().SetRotationXDegree(TimeAngle);
-	GetTransform().SetRotationYDegree(TimeAngle);
-	GetTransform().SetRotationZDegree(TimeAngle);
 }
 
 void Otus::End()
